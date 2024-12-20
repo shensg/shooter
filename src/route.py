53fdc6c9080f942f613fb74872908e95f99739e0
you name <you@example.com>
2024-12-20 17:28:29 +0800

    add: new route

--- src/route.py
+++ src/route.py
@@ -3,16 +3,19 @@
 from src.handler.index import IndexHandler
 from src.handler import LoginHandler
 from src.handler.userHandler import UserAdd, UserModify, UserDelete, UserGet
-from src.handler.cdnHandler import RefreshAliyunCdn, RefreshCloudFlareCdn
+from src.handler.cdnHandler import RefreshAliyunCdn, RefreshCloudFlareCdn, GetRefreshAliyunHistory, \
+    GetRefreshCloudFlareHistory
 
 application_route = [
     (r"/", IndexHandler),
     (r"/login", LoginHandler),
-    (r"/user/add",UserAdd),
-    (r"/user/modify",UserModify),
-    (r"/user/delete",UserDelete),
-    (r"/user/getcurrent",UserGet),
-    (r"/refresh/aliyun",RefreshAliyunCdn),
-    (r"/refresh/cloudflare",RefreshCloudFlareCdn),
+    (r"/user/add", UserAdd),
+    (r"/user/modify", UserModify),
+    (r"/user/delete", UserDelete),
+    (r"/user/getcurrent", UserGet),
+    (r"/refresh/aliyun", RefreshAliyunCdn),
+    (r"/refresh/cloudflare", RefreshCloudFlareCdn),
+    (r"/refresh/record/aliyun", GetRefreshAliyunHistory),
+    (r"/refresh/record/cloudflare", GetRefreshCloudFlareHistory),
 
 ]
