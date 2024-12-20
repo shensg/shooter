762f9c9b4bd1021c77da3c942a33e33d9af7b782
you name <you@example.com>
2024-12-20 17:26:36 +0800

    update: add new table

--- create-table.py
+++ create-table.py
@@ -2,9 +2,10 @@
 
 from src.models import connect_db, close_db, db
 from src.models.sys import User, Group, Route, Secrets
+from src.models.refresh_cdn import RefreshHistory
 
 
 db = db
 connect_db()
-db.create_tables([User, Group, Route, Secrets])
+db.create_tables([User, Group, Route, Secrets, RefreshHistory])
 close_db()
